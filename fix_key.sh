884b2da8c0ce54112aefd71eefffd0d4a0b9a1ab
Shaman POS <shaman@pos.com>
2026-07-14 14:29:15 +0300

    chore: Remove temporary fix files

--- fix_key.sh
+++
@@ -1,4 +0,0 @@
-KEY=$(openssl genrsa 2048 | awk '1' ORS='\n')
-echo "RSA_PRIVATE_KEY="$KEY"" >> /var/www/serenut/server/.env.production
-cd /var/www/serenut/server
-docker compose -f docker-compose.prod.yml restart backend
